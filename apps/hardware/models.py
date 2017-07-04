11b377bd0573dd9ceaa15134d1ba2c00aaab62ea
Brady Endres <bfendres@gmail.com>
2017-07-03 22:54:45 -0700

    Fixed wsgi file

--- apps/hardware/models.py
+++ apps/hardware/models.py
@@ -4,9 +4,9 @@ from config.settings import app, db
 class Hardware(db.Model):
 	id = db.Column('hardware_id', db.Integer, primary_key = True)
 	name = db.Column(db.String(100))
-	sn = db.Column(db.String(100))  
-	company = db.Column(db.String(200))
-	model = db.Column(db.String(10))
+	sn = db.Column(db.String(50))  
+	company = db.Column(db.String(100))
+	model = db.Column(db.String(60))
 	
 	def __init__(self, name, company, model, sn):
 		self.name = name
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from config.settings import app
+from app import app
 
 if __name__ == "__main__":
 	app.run()
